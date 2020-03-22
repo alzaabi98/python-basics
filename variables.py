2d799f0dfa0036318ebdb02eeb96e51f5cf22266
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2020-03-22 19:34:41 +0400

    variables lessson

---
+++ variables.py
@@ -0,0 +1,45 @@
+# This is single line comemnts
+# this is help file
+
+"""
+Data Types
+integer : like 10 , 20, 1, -20
+flaot: like 10.3, 3.5, -5.5
+string: like Ahmed , good morning
+bool: True ,False
+"""
+
+# print(10)
+# print(-10)
+# print(10.2, 100.3)
+# print('Ahmed', 'good morning')
+
+'''
+variable :
+ 1- start with letter or undescor
+ 2- mix of letters and numbers 
+ 3-cant stat with num
+'''
+
+# age = 40
+# print(age)
+# print(type(age))
+# name = "Abdulaziz"
+# print(name)
+
+firstname = "Abdulaziz"
+lastname = "Alzaabi"
+# + for concatenate strings
+# print(firstname + " " + lastname)
+
+# age = 40
+# print(firstname + str(age))
+
+# print("Ahmed" * 5)
+
+# isCold = True
+# print(isCold)
+firstname = "Ali"
+print(firstname)
+
+yourname = input("What is your name ? ")
