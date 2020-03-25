3b57cbeeba176b42ba22299dc31cf54a8cc26d37
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2020-03-25 12:24:59 +0400

    add if-statement file

---
+++ if_statement.py
@@ -0,0 +1,68 @@
+"""
+comparsions symobols
+=======================
+== : equal
+!= : does not equal
+< : less than
+> : larger than
+
+And Operator 
+=================
+True and True  =>  True
+True and False  =>  False
+False and True  =>  False
+False and False  =>  False
+
+or Operator 
+=================
+True or True  =>  True
+True or False  =>  True
+False or True  =>  True
+False or False  =>  False
+
+"""
+
+
+# if else
+# name = "Ahmed"
+# if name == "Ahmed":
+#     print("your name is Ahmed")
+#     print("welcome to your website")
+# else:
+#     print("i dont know your name")
+
+
+# if elif else
+# age = 40
+
+# if age == 20:
+#     print("your age is 20")
+# elif age == 25:
+#     print("your age is 25")
+# else:
+#     print("i dont know your age")
+
+
+# logical comparsions
+
+# name2 = "Ahmed"
+# age2 = 10
+# if name2 == "Ali" or age2 > 15:
+#     print('true')
+# else:
+#     print('false')
+
+# Ternary  operator
+
+# name3 = "salem"
+# print('yes') if name3 == "salem" else print("no")
+
+
+# pass - means placeholer to write code later
+
+age3 = 40
+
+if age3 < 40:
+    pass
+
+print("not in if statement")
