5531b535530887adf630f4755a59e1b11d8266b6
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2020-03-26 17:12:34 +0400

    add loops.py file

---
+++ loops.py
@@ -0,0 +1,40 @@
+# while loop
+# x = 0
+
+# while x < 10:
+#     print(x)
+#     x = x + 1
+# print('code ended . Thank you')
+
+
+# while with !=
+# name = ""
+# while name != "guest":
+#     name = input('enter a name ')
+
+# while loop with break
+
+# while True:
+#     name2 = input()
+#     if name2 == "Ahmed":
+#         break
+# print('completd')
+
+# for loop and range
+
+# for i in range(5):
+#     print(i)
+
+# for loop with start and end
+
+# for i in range(5, 10):
+#     print(i)
+
+# for loop with step
+
+# for i in range(0, 10, 4):
+#     print(i)
+# for loop - minus
+
+for i in range(10, 0, -2):
+    print(i)
