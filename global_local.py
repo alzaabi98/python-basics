6252a6c1c627a13ebc7133a458340bebaff4afb7
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2020-03-30 12:05:05 +0400

    add global_local.py file

---
+++ global_local.py
@@ -0,0 +1,24 @@
+# variables in python can be local or global
+# it is important to know the differnce
+
+# local scope example
+# if  you want to access variable from all program keep it global
+# if you to change inside function- add global variable_name
+
+age = 20
+
+
+def myage():
+    age = 14
+    name = "Ahmed"
+    print(age)
+
+
+def myage2():
+    global age
+    age = 30
+
+
+myage()
+myage2()
+print(age)
